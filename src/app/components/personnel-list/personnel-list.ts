037dd4f1016e656a7c1a70d2a7184d89c31706be
Vicky Lee Jones <73119731+vlj8008@users.noreply.github.com>
2026-09-01 15:37:18 -0500

    Refactor Personnel tab into its own standalone component

---
+++ src/app/components/personnel-list/personnel-list.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-personnel-list',
+  imports: [MatIconModule],
+  templateUrl: './personnel-list.html',
+  styleUrl: './personnel-list.css',
+})
+export class PersonnelList {}
